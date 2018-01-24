62caf3f462bc64796261e92e7de864db4ce8b466
Jamie Higgins <S00162685@mail.itsligo.ie>
2018-01-24 12:48:04 +0000

    Exercise 2.2

---
+++ WebApplication1/Models/Metadata.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class CustomerMetadata
+    {
+        public int Id;
+
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(25, MinimumLength = 2)]
+        public string FirstName;
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(25, MinimumLength = 2)]
+        public string LastName;
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [EmailAddress]
+        public string Email;
+    }
+
+    [MetadataType(typeof(CustomerMetadata))]
+    public partial class Customer { }
+
+    public class OrderMetadata
+    {
+        public int Id;
+        
+        public int CustomerId;
+        
+        public int ProductId;
+        
+        public int OrderQuantity;
+    }
+
+    [MetadataType(typeof(OrderMetadata))]
+    public partial class Order { }
+
+    public class ProductMetadata
+    {
+        public int Id;
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(25, MinimumLength = 2)]
+        public string Name;
+
+        [Required(ErrorMessage = "Product Cost is required")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal Cost;
+
+        [Required(ErrorMessage = "Product quantity is required")]
+        [Range(0, 100, ErrorMessage = "Quanity can only be between 0 and 100")]
+        public int Quantity;
+    }
+
+    [MetadataType(typeof(ProductMetadata))]
+    public partial class Product { }
+}
